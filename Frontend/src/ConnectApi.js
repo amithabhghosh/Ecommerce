017deeb696d1d400fa124d1e9c2c92f24589a3ad
amithabhghosh <amithabhghosh@gmail.com>
2025-02-07 14:42:05 +0530

    backend code updated

--- Frontend/src/ConnectApi.js
+++ Frontend/src/ConnectApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://ecommercebackend-tau-five.vercel.app",
     withCredentials: true,  
 });
 
